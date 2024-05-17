21933ccfa96991ef566a6bae9f8a1650f773f94e
manhnk <manhnguyentth@gmail.com>
2024-05-17 08:40:28 +0700

    add custom opr

--- dags/example_dynamic_task_mapping.py
+++ dags/example_dynamic_task_mapping.py
@@ -5,7 +5,32 @@ from datetime import datetime
 from airflow.decorators import task, dag
 from airflow.operators.python import get_current_context
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.models.baseoperator import BaseOperator
 
+
+class AddOneOperator(BaseOperator):
+    """A custom operator that adds one to the input."""
+
+    def __init__(self, value, **kwargs):
+        super().__init__(**kwargs)
+        self.value = value
+
+    def execute(self, context):
+        return self.value + 1
+    
+class SumItOperator(BaseOperator):
+    """A custom operator that sums the input."""
+
+    template_fields = ("values",)
+
+    def __init__(self, values, **kwargs):
+        super().__init__(**kwargs)
+        self.values = values
+
+    def execute(self, context):
+        total = sum(self.values)
+        print(f"Total was {total}")
+        return total
 
 @dag(
     dag_id="example_dynamic_task_mapping",
@@ -23,10 +48,15 @@ def example_dynamic_task_mapping():
         return [1, 2, {"a": "b"}, "str"]
 
     @task(max_active_tis_per_dagrun=2)
-    def consumer(arg):
-        print(arg)
+    def consumer(arg1, arg2):
+        print(arg1)
+        print(arg2)
 
-    consumer.expand(arg=make_list())
+    consumer.partial(arg2='constant').expand(arg1=make_list())
+
+    add_one_task = AddOneOperator.partial(task_id="add_one").expand(value=[1, 2, 3])
+    sum_it_task = SumItOperator(task_id="sum_it", values=add_one_task.output)
+    
     # SQLExecuteQueryOperator.partial(
     #     task_id="fetch_data",
     #     sql="SELECT * FROM data WHERE date = %(date)s",
